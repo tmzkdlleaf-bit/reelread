87e01be5c860fa5f597a043edad03eaa886ff5f9
tmzkdlleaf <tmzkdlleaf@gmail.com>
2026-05-23 23:31:04 +0900

    PWA 추가

---
+++ public/sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'reelread-v1'
+const STATIC_ASSETS = ['/', '/index.html']
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', e => {
+  // Firebase, Google 등 외부 요청은 네트워크 우선
+  if (!e.request.url.startsWith(self.location.origin)) return
+
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone()
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone))
+        return res
+      })
+      .catch(() => caches.match(e.request))
+  )
+})
